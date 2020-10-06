ca53491e4504939f6f4a56c79455d0d5304f8d40
Vinny Nyamu <vincenyamu@gmail.com>
2020-10-05 23:39:32 -0400

    added css animations on page load

---
+++ src/js/base.js
@@ -0,0 +1,15 @@
+// HTML Selectors
+
+export const elements = {
+  logo: document.querySelector('.logo'),
+  header: document.querySelector('header'),
+  pageLinksItem: document.querySelectorAll('.pageLinks__item'),
+  socials: document.querySelector('.socials'),
+  socialsList: document.querySelector('.socials__list'),
+  socialsItem: document.querySelectorAll('.socials__item'),
+  about: document.querySelector('.about'),
+  aboutTitle: document.querySelector('.about__title'),
+  aboutSection: document.querySelector('.about__section'),
+};
+
+// const logo = document.querySelector('.logo');
---
+++ src/js/index.js
@@ -0,0 +1,53 @@
+import { gsap } from 'gsap';
+import Glide from '@glidejs/glide';
+import { elements } from './base';
+import { CSSRulePlugin } from 'gsap/CSSRulePlugin';
+
+// LOGO Animation
+const logoAnimation = gsap.timeline();
+
+logoAnimation.from([elements.logo], {
+  duration: 1,
+  y: 40,
+  opacity: 0,
+  ease: 'power3.out',
+});
+
+// HEADER ANIMATION
+const initialAnimation = gsap.timeline();
+
+initialAnimation
+  .from([elements.pageLinksItem], {
+    duration: 0.5,
+    opacity: -1,
+    x: 0,
+    ease: 'power3.inOut',
+    stagger: {
+      amount: 0.5,
+    },
+  })
+  .from([elements.socialsList], {
+    duration: 0.5,
+    opacity: 0,
+    // width: 0,
+    x: 0,
+    y: 20,
+    ease: 'power3.inOut',
+  })
+  .from([elements.socialsItem], {
+    duration: 0.5,
+    opacity: -1,
+    x: 0,
+    y: 20,
+    ease: 'power3.inOut',
+  })
+  .from([elements.aboutTitle, elements.aboutSection], {
+    duration: 0.5,
+    opacity: -1,
+    y: 40,
+    // x: 0,
+    ease: 'power3.inOut',
+    stagger: {
+      amount: 0.5,
+    },
+  });
