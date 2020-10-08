e9e5694e0c20efca4d9f6f16c02d13121ed3b0bc
Vinny Nyamu <vincenyamu@gmail.com>
2020-10-08 01:03:49 -0400

    updated site links. site finalized

--- src/js/index.js
+++ src/js/index.js
@@ -1,52 +1,51 @@
-// import { gsap } from 'gsap';
-// import { elements } from './base';
-// import { CSSRulePlugin } from 'gsap/CSSRulePlugin';
+import { gsap } from 'gsap';
+import { elements } from './base';
 
-// // LOGO Animation
-// const logoAnimation = gsap.timeline();
+// LOGO Animation
+const logoAnimation = gsap.timeline();
 
-// logoAnimation.from([elements.logo], {
-//   duration: 1,
-//   y: 40,
-//   opacity: 0,
-//   ease: 'power3.out',
-// });
+logoAnimation.from([elements.logo], {
+  duration: 1,
+  y: 40,
+  opacity: 0,
+  ease: 'power3.out',
+});
 
-// // HEADER ANIMATION
-// const initialAnimation = gsap.timeline();
+// HEADER ANIMATION
+const initialAnimation = gsap.timeline();
 
-// initialAnimation
-//   .from([elements.pageLinksItem], {
-//     duration: 0.5,
-//     opacity: -1,
-//     x: 0,
-//     ease: 'power3.inOut',
-//     stagger: {
-//       amount: 0.5,
-//     },
-//   })
-//   .from([elements.socialsList], {
-//     duration: 0.5,
-//     opacity: 0,
-//     // width: 0,
-//     x: 0,
-//     y: 20,
-//     ease: 'power3.inOut',
-//   })
-//   .from([elements.socialsItem], {
-//     duration: 0.5,
-//     opacity: -1,
-//     x: 0,
-//     y: 20,
-//     ease: 'power3.inOut',
-//   })
-//   .from([elements.aboutTitle, elements.aboutSection], {
-//     duration: 0.5,
-//     opacity: -1,
-//     y: 40,
-//     // x: 0,
-//     ease: 'power3.inOut',
-//     stagger: {
-//       amount: 0.5,
-//     },
-//   });
+initialAnimation
+  .from([elements.pageLinksItem], {
+    duration: 0.5,
+    opacity: -1,
+    x: 0,
+    ease: 'power3.inOut',
+    stagger: {
+      amount: 0.5,
+    },
+  })
+  .from([elements.socialsList], {
+    duration: 0.5,
+    opacity: 0,
+    // width: 0,
+    x: 0,
+    y: 20,
+    ease: 'power3.inOut',
+  })
+  .from([elements.socialsItem], {
+    duration: 0.5,
+    opacity: -1,
+    x: 0,
+    y: 20,
+    ease: 'power3.inOut',
+  })
+  .from([elements.aboutTitle, elements.aboutSection], {
+    duration: 0.5,
+    opacity: -1,
+    y: 40,
+    // x: 0,
+    ease: 'power3.inOut',
+    stagger: {
+      amount: 0.5,
+    },
+  });
